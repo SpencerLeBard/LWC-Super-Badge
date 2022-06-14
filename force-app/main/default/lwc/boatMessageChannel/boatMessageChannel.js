3038d332dcccb4c003e9af031b0de3c342894219
spencer lebard <slebard@gmail.com>
2022-06-13 17:27:13 -0700

    boatsboatsboats

--- force-app/main/default/lwc/boatMessageChannel/boatMessageChannel.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement } from 'lwc';
-
-import {  } from 'lightning/messageService';
-//import BOATMC from '@salesforce/...';
-
-export default class BoatMessageChannel extends LightningElement {
-
-
-}
